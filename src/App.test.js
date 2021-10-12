add3afa3ca4c2c7e22f210e0dd1e8bacd46383bc
David Castillo <davidcastillo@davids-air.lan>
2021-10-11 19:42:53 -0500

    address npm issue with svelte

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,11 @@
 import { render, screen } from '@testing-library/react';
-import { fireEvent } from '@testing-library/svelte';
+import userEvent from '@testing-library/user-event'
 import App from './App';
 
 test('renders text when button is clicked', () => {
   render(<App />);
   const button = screen.getByText(/Click me to add or remove text/i);
-  fireEvent.click(button)
+  userEvent.click(button)
   const text = screen.getByText(/Thanks for the opportunity!/i)
   expect(text).toBeInTheDocument();
 });
