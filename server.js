938a0956ec17ed91448a0d47b46d4b48022c0b48
David Castillo <davidcastillo@davids-air.lan>
2021-10-11 20:42:18 -0500

    remove unused files/packages

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 5000;
-
-app.listen(port, () => console.log(`listening on port ${port}`));
-
-app.get('/home', (req, res) => {
-    res.send({express: "Connected"})
-})
