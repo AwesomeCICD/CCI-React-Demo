e354ec2f88ccd8de57ec6322f5820a46c4e05c7b
David Castillo <d.v.castillo37@gmail.com>
2022-01-10 16:56:54 -0600

    update src file config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './Header';
+import Footer from './Footer';
 import { Button, Typography } from '@material-ui/core';
 
 function hasClicked(x) {
