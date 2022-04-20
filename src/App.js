721dc918f984d01271c2eb92bf5ea95e983d120c
David Castillo <d.v.castillo37@gmail.com>
2022-04-20 12:34:33 -0500

    demo

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <div className="body">
         {click ? (
           <Typography variant="h2" gutterBottom>
-            Thanks for clicking
+            Thanks for stopping by
           </Typography>
         ) : (
           <Typography variant="h2" gutterBottom>
