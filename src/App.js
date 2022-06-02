68b043e6d360442295f358dbddcb2e020856b033
David Castillo <d.v.castillo37@gmail.com>
2022-06-02 15:10:10 -0400

    demo

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
           </Typography>
         ) : (
           <Typography variant="h2" gutterBottom>
-            What a cool app
+            What a cool demo
           </Typography>
         )}
         <Button
