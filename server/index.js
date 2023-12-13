9e41f582207850fba6f829f65f39abd721adb0a2
Derry Bradley <derrybradley@live.ie>
2023-12-13 09:27:13 -0500

    app change

--- server/index.js
+++ server/index.js
@@ -4,8 +4,12 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "..", "build")));
 
+// app.use((req, res) => {
+//   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+// });
+
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+  res.sendFile(__dirname + 'public/index.html');
 });
 
 app.listen(3000, () => {
